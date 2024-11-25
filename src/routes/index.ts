860294472481745337335d5167611c687f58c2e8
Raden Sukmana <135584291+Radensukmana21@users.noreply.github.com>
2024-11-26 02:04:20 +0700

    Add files via upload

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,1 +1,19 @@
+import { Router } from "express";
+import { healthcheck } from "../controllers/healthcheck.controller";
+import multer from "multer";
+import { getPredictionHistories, predict } from "../controllers/predict.controller";
 
+const router = Router();
+
+// Multer setup
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: { fileSize: 1000000 },
+});
+
+// Routes
+router.get("/healthcheck", healthcheck);
+router.post("/predict", upload.single("image"), predict);
+router.get("/predict/histories", getPredictionHistories); // Added route for fetching prediction histories
+
+export default router;
